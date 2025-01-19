21a6c833959afb5bec592ade39b69a0aad12fa7a
alexandre <zatti.alexandre61@gmail.com>
2025-01-19 18:12:03 -0300

    Adding new image picker dependencies

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,7 +1,24 @@
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
+import { useFilePicker } from "use-file-picker";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const {openFilePicker, filesContent} = useFilePicker({
+    readAs: 'BinaryString',
+    accept: 'image/*',
+    multiple: false,
+  });
+
   return (
-    <div>
-      <h1>Diabo loro</h1>
+    <div className={cn('flex flex-col justify-center items-center gap-5')}>
+      <h1 className={cn('text-4xl')}>Groceries Analyzer</h1>
+      <Separator className={cn('w-3/4')}/>
+      <Button onClick={openFilePicker}>Pick Receipt Image</Button>
+      {filesContent.map((file) => (
+        <img key={file.name} className={cn('w-3/4 h-3/4')} alt={'receipt'} src={file.content}/>
+      ))}
     </div>
   )
 }
