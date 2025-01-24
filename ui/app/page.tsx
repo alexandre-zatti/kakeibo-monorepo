05fe038fa5fd09934c7a4ce6486dd83e2f7436cd
alexandre <zatti.alexandre61@gmail.com>
2025-01-23 22:36:51 -0300

    Adding initial prototype of home page with receipt upload functionality

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,24 +1,54 @@
+"use client"
+
 import { cn } from "@/lib/utils";
-import { Separator } from "@/components/ui/separator";
 import React from "react";
 import { useFilePicker } from "use-file-picker";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { ImageUp } from "lucide-react";
 
 export default function Home() {
-  const {openFilePicker, filesContent} = useFilePicker({
+  const {openFilePicker} = useFilePicker({
     readAs: 'BinaryString',
     accept: 'image/*',
     multiple: false,
+    onFilesSuccessfullySelected: async ({plainFiles, filesContent}) => {
+      console.log('onFilesSuccessfullySelected', plainFiles, filesContent);
+      await processReceiptImage(plainFiles[0])
+    },
   });
 
+  async function processReceiptImage(file: File) {
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+      const res = await fetch('https://groceries.zatti.tech/api/receipt/process', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'X-API-KEY': ''
+        }
+      })
+
+      const jsonRes = await res.json()
+
+      console.log(jsonRes)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className={cn('flex flex-col justify-center items-center gap-5')}>
-      <h1 className={cn('text-4xl')}>Groceries Analyzer</h1>
+    <div className={cn('flex flex-col justify-center items-center gap-5 h-screen')}>
+      <Image src={'/sales-graph.png'} alt={'Shopping cart with sales data graph'} width={150} height={150}/>
+      <h1 className={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl')}>Groceries Analyzer</h1>
       <Separator className={cn('w-3/4')}/>
-      <Button onClick={openFilePicker}>Pick Receipt Image</Button>
-      {filesContent.map((file) => (
-        <img key={file.name} className={cn('w-3/4 h-3/4')} alt={'receipt'} src={file.content}/>
-      ))}
+      <Button size={'lg'} onClick={openFilePicker}>
+        <ImageUp/>
+        Upload Receipt
+      </Button>
     </div>
-  )
+  );
 }
