532a4f4296e7db7b3bf64db722b894d557161706
alexandre <zatti.alexandre61@gmail.com>
2025-01-27 20:43:36 -0300

    Setup server render home page with next server action for form submit

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,54 +1,31 @@
-"use client"
-
 import { cn } from "@/lib/utils";
 import React from "react";
-import { useFilePicker } from "use-file-picker";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
+import { LabelButton } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import { ImageUp } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import processReceipt from "@/app/actions/processReceipt";
 
 export default function Home() {
-  const {openFilePicker} = useFilePicker({
-    readAs: 'BinaryString',
-    accept: 'image/*',
-    multiple: false,
-    onFilesSuccessfullySelected: async ({plainFiles, filesContent}) => {
-      console.log('onFilesSuccessfullySelected', plainFiles, filesContent);
-      await processReceiptImage(plainFiles[0])
-    },
-  });
-
-  async function processReceiptImage(file: File) {
-    const formData = new FormData()
-    formData.append("file", file)
-
-    try {
-      const res = await fetch('https://groceries.zatti.tech/api/receipt/process', {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'X-API-KEY': ''
-        }
-      })
-
-      const jsonRes = await res.json()
-
-      console.log(jsonRes)
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return (
-    <div className={cn('flex flex-col justify-center items-center gap-5 h-screen')}>
-      <Image src={'/sales-graph.png'} alt={'Shopping cart with sales data graph'} width={150} height={150}/>
-      <h1 className={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl')}>Groceries Analyzer</h1>
-      <Separator className={cn('w-3/4')}/>
-      <Button size={'lg'} onClick={openFilePicker}>
-        <ImageUp/>
-        Upload Receipt
-      </Button>
+    <div className={cn("flex flex-col justify-center items-center gap-5 h-screen")}>
+      <Image src={"/sales-graph.png"} alt={"Shopping cart with sales data graph"} width={150} height={150}
+             priority={true}/>
+      <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl")}>Groceries Analyzer</h1>
+      <Separator className={cn("w-3/4")}/>
+      <form action={processReceipt} className={cn("flex flex-col justify-center items-center gap-5")}>
+        <LabelButton htmlFor="receipt">
+          <ImageUp/>
+          Upload Receipt
+        </LabelButton>
+        <Input id="receipt" name={"file"} type="file" className={cn("hidden")}/>
+        <Button type={"submit"} size={"lg"}>
+          Process
+        </Button>
+      </form>
     </div>
   );
 }
