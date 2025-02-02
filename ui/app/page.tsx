2b2fda28b63799da94ede70c37785c9ea832877c
alexandre <zatti.alexandre61@gmail.com>
2025-02-02 16:46:43 -0300

    Created new client component to deal with file upload and process buttons, added validation and loading state

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -2,11 +2,8 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
-import { LabelButton } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { ImageUp } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import processReceipt from "@/app/actions/processReceipt";
+import { UploadReceipt } from "@/components/uploadReceipt";
 
 export default function Home() {
 
@@ -17,14 +14,7 @@ export default function Home() {
       <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl")}>Groceries Analyzer</h1>
       <Separator className={cn("w-3/4")}/>
       <form action={processReceipt} className={cn("flex flex-col justify-center items-center gap-5")}>
-        <LabelButton htmlFor="receipt">
-          <ImageUp/>
-          Upload Receipt
-        </LabelButton>
-        <Input id="receipt" name={"file"} type="file" className={cn("hidden")}/>
-        <Button type={"submit"} size={"lg"}>
-          Process
-        </Button>
+        <UploadReceipt/>
       </form>
     </div>
   );
