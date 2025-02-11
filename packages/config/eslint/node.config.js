5562ee26ed13f18b6d2ce032d84a7bf5b579eecf
alexandre <zatti.alexandre61@gmail.com>
2025-02-11 18:58:30 -0300

    tsconfig and dependencies fix

--- packages/config/eslint/node.config.js
+++ packages/config/eslint/node.config.js
@@ -4,6 +4,9 @@ export default [
     ...baseConfig,
     {
         files: ["**/*.ts", "**/*.js"],
+        rules: {
+            "@typescript-eslint/no-floating-promises": "warn",
+        }
     }
 ]
 
