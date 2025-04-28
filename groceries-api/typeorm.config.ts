a7eb679936264970d1e2b7888e79cf2735b35642
alexandre <zatti.alexandre61@gmail.com>
2025-04-28 08:40:59 -0300

    Updating docker compose

--- groceries-api/typeorm.config.ts
+++ groceries-api/typeorm.config.ts
@@ -4,6 +4,7 @@ export default new DataSource({
   type: 'sqlite',
   database: './db/data/database.sqlite',
   synchronize: false,
+  migrationsRun: true,
   entities: ['./dist/**/*.entity.js'],
   migrations: ['./dist/db/migrations/*.js'],
 });
